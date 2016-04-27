3edbd68b55070f1d8391aaa8ec33e9c6d1f1eff3
lukas <lukas.bitter@gmail.com>
2016-04-27 09:32:10 +0200

    initial commit

---
+++ SpreadPoll.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World! flask hello'
+
+if __name__ == '__main__':
+    app.run(debug=True) # désactiver le debug en production!!!
